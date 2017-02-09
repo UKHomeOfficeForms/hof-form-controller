3b5f07392150b0dc737272bbeb02e16a53d183cc
joefitter <hello@joefitter.com>
2017-02-09 11:21:05 +0000

    Refactor - integrate hof controllers
    * added base-controller from hof-controllers as default export. Extending from what was previously /lib/form.js
    * added error from hof-controllers as default error
    * Fixed bug in tests where form.formatters were being mutated - these can now be set on req.form.options.formatters
    * added hof-behaviour-session and hof-behaviour-hooks - mixin to exported controller
    * Removed lint for now as mix of es5/es6

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var Controller = require('./lib/controller');
+'use strict';
 
-Controller.validators = require('./lib/validation/validators');
-Controller.formatters = require('./lib/formatting/formatters');
+const Session = require('hof-behaviour-session');
+const Hooks = require('hof-behaviour-hooks');
+const Controller = require('./lib/controller');
+const mix = require('mixwith').mix;
 
-Controller.Error = require('./lib/error');
+class FormController extends mix(Controller).with(Session, Hooks) {}
 
-module.exports = Controller;
+FormController.validators = require('./lib/validation/validators');
+FormController.formatters = require('./lib/formatting/formatters');
+
+FormController.Error = require('./lib/error');
+
+module.exports = FormController;
+module.exports.Controller = Controller;
+module.exports.BaseController = require('./lib/base-controller');
+module.exports.BaseError = require('./lib/base-error');
