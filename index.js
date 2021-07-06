dacca7cbfb237c2415fda258ba8548d169ba34ef
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2021-07-06 17:54:59 +0100

    Add Deprecated Form and update to 6.4.0

--- index.js
+++ index.js
@@ -3,16 +3,20 @@
 const Session = require('hof-behaviour-session');
 const Hooks = require('hof-behaviour-hooks');
 const Controller = require('./lib/controller');
+const DeprecatedForm = require('./lib/deprecated-form');
 const mix = require('mixwith').mix;
 
 class FormController extends mix(Controller).with(Session, Hooks) {}
 
 FormController.validators = require('./lib/validation/validators');
 FormController.formatters = require('./lib/formatting/formatters');
+DeprecatedForm.validators = require('./lib/validation/validators');
+DeprecatedForm.formatters = require('./lib/formatting/formatters');
 
 FormController.ValidationError = require('./lib/validation-error');
-FormController.Error = require('./lib/deprecated-error');
+DeprecatedForm.Error = require('./lib/deprecated-error');
 
 module.exports = FormController;
+module.exports.DeprecatedForm = DeprecatedForm;
 module.exports.Controller = Controller;
 module.exports.BaseController = require('./lib/base-controller');
